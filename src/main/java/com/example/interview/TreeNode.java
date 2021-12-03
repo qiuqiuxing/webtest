2050269c47c878cb262fed8a84bb73b416bbf145
邱容 <2991255829@qq.com>
2021-12-04 00:23:52 +0800

    算法题

---
+++ src/main/java/com/example/interview/TreeNode.java
@@ -0,0 +1,68 @@
+package com.example.interview;
+
+public class TreeNode {
+    String val;
+    //左子树
+    TreeNode left;
+    //右子树
+    TreeNode right;
+
+    //构造方法
+    TreeNode(String x) {
+        this.val = x;
+    }
+    // 递归先序遍历
+    public static void recursionPreorderTraversal(TreeNode root) {
+        if (root != null) {
+            System.out.print(root.val + " ");
+            recursionPreorderTraversal(root.left);
+            recursionPreorderTraversal(root.right);
+        }
+    }
+    // 递归中序遍历
+    public static void recursionMiddleorderTraversal(TreeNode root) {
+        if (root != null) {
+            recursionMiddleorderTraversal(root.left);
+            System.out.print(root.val + " ");
+            recursionMiddleorderTraversal(root.right);
+        }
+    }
+
+
+
+    // 递归后序遍历
+    public static void recursionPostorderTraversal(TreeNode root) {
+        if (root != null) {
+            recursionPostorderTraversal(root.left);
+            recursionPostorderTraversal(root.right);
+            System.out.print(root.val + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode A = new TreeNode("A");
+        TreeNode B = new TreeNode("B");
+        TreeNode C = new TreeNode("C");
+        TreeNode D = new TreeNode("D");
+        TreeNode E = new TreeNode("E");
+        TreeNode F = new TreeNode("F");
+        TreeNode G = new TreeNode("G");
+        TreeNode H = new TreeNode("H");
+        TreeNode I = new TreeNode("I");
+        TreeNode J = new TreeNode("J");
+        TreeNode K = new TreeNode("K");
+        A.left = B;
+        A.right = C;
+        B.left = D;
+        B.right = E;
+        C.left = F;
+        C.right = G;
+        D.left = H;
+        D.right = I;
+        E.right = J;
+        F.right = K;
+        recursionPreorderTraversal( A);
+        recursionMiddleorderTraversal( A);
+        recursionPostorderTraversal( A);
+    }
+}
